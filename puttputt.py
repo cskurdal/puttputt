7e4a9f62ee1940bd5bacd8e10d90fd032737668c
Caleb Skurdal <cskurdal@users.noreply.github.com>
2018-10-28 21:00:48 -0700

    Update puttputt.py

--- puttputt.py
+++ puttputt.py
@@ -36,7 +36,7 @@ def runMotorThread(motor, start, maxtime, numStepsPerLoop = 1):
     
     while (time.time() - start) <= maxtime:
         #TODO: maybe use queue based events as described here: https://www.raspberrypi.org/forums/viewtopic.php?t=178212
-		#moveSteps(motor, 1*stepsPerRev)
+        #moveSteps(motor, 1*stepsPerRev)
         motor.step(numStepsPerLoop, delay = delay, turnOff = False)
     
     motor.turnOff()
@@ -83,128 +83,8 @@ def main():
     #thread2.start()
     
 #---------------------------------------------------
-if isRpi:
-	def setStep1(w1, w2, w3, w4):
-		GPIO.output(motor1_A_1_pin, w1)
-		GPIO.output(motor1_B_1_pin, w2)
-		GPIO.output(motor1_A_2_pin, w3)
-		GPIO.output(motor1_B_2_pin, w4)
-
-if isRpi:
-	def moveSteps(motor, numSteps, turnOff = True):
-		try:
-			global currMotor1Step, currMotor2Step, delay1, delay2, reverseMotor1, reverseMotor2
-			numSteps = int(round(numSteps))
-			#currMotor1Step = 0b1000
-			#currMotor2Step = 0b1000
-#			print 'moving motor:', motor, ' steps:', numSteps, ' rev1:', reverseMotor1, ' rev2:', reverseMotor2, ' delay1:', delay1, ' delay2:', delay2
-
-			if motor == 1 and ((numSteps > 0 and not reverseMotor1) or\
-					(numSteps < 0 and reverseMotor1) ): #Motor 1 spinning +
-				for i in range(0, abs(numSteps)):
-					#Set next step
-					currMotor1Step = currMotor1Step>>1
-					if currMotor1Step == 0b0000:
-						currMotor1Step = 0b1000
-
-					p1 = (currMotor1Step>>3)&1
-					p2 = (currMotor1Step>>2)&1
-					p3 = (currMotor1Step>>1)&1
-					p4 = currMotor1Step&1
-					
-					#print '    output to motor 1+',p1,p2,p3,p4
-					GPIO.output(motor1_A_1_pin, p1)
-					GPIO.output(motor1_B_1_pin, p2)
-					GPIO.output(motor1_A_2_pin, p3)
-					GPIO.output(motor1_B_2_pin, p4)
-					
-					time.sleep(delay1)
-
-			elif motor == 1 and ((numSteps > 0 and reverseMotor1) or\
-					(numSteps < 0 and not reverseMotor1) ): #Motor 1 spinning -
-				for i in range(0, abs(numSteps)):
-					#Set next step
-					currMotor1Step = currMotor1Step>>1
-					if currMotor1Step == 0b0000:
-						currMotor1Step = 0b1000
-
-					p1 = currMotor1Step&1
-					p2 = (currMotor1Step>>1)&1
-					p3 = (currMotor1Step>>2)&1
-					p4 = (currMotor1Step>>3)&1
-
-					#print '    output to motor 1-',p1,p2,p3,p4
-					GPIO.output(motor1_A_1_pin, p1)
-					GPIO.output(motor1_B_1_pin, p2)
-					GPIO.output(motor1_A_2_pin, p3)
-					GPIO.output(motor1_B_2_pin, p4)
-					
-					time.sleep(delay1)
-			elif motor == 2 and ((numSteps > 0 and not reverseMotor2) or\
-					(numSteps < 0 and reverseMotor2) ): #Motor 2 spinning +
-				for i in range(0, abs(numSteps)):
-					#Set next step
-					currMotor2Step = currMotor2Step>>1
-					if currMotor2Step == 0b0000:
-						currMotor2Step = 0b1000
-
-					p1 = (currMotor2Step>>3)&1
-					p2 = (currMotor2Step>>2)&1
-					p3 = (currMotor2Step>>1)&1
-					p4 = currMotor2Step&1
-
-					#print '    output to motor 2+        ',p1,p2,p3,p4
-					GPIO.output(motor2_A_1_pin, p1)
-					GPIO.output(motor2_B_1_pin, p2)
-					GPIO.output(motor2_A_2_pin, p3)
-					GPIO.output(motor2_B_2_pin, p4)
-				
-					time.sleep(delay2)
-			elif motor == 2 and ((numSteps > 0 and reverseMotor2) or\
-					(numSteps < 0 and not reverseMotor2) ): #Motor 2 spinning -
-				for i in range(0, abs(numSteps)):
-					#Set next step
-					currMotor2Step = currMotor2Step>>1
-					if currMotor2Step == 0b0000:
-						currMotor2Step = 0b1000
-
-					p1 = currMotor2Step&1
-					p2 = (currMotor2Step>>1)&1
-					p3 = (currMotor2Step>>2)&1
-					p4 = (currMotor2Step>>3)&1
-
-					#print '    output to motor 2-        ',p1,p2,p3,p4
-					GPIO.output(motor2_A_1_pin, p1)
-					GPIO.output(motor2_B_1_pin, p2)
-					GPIO.output(motor2_A_2_pin, p3)
-					GPIO.output(motor2_B_2_pin, p4)
-
-					time.sleep(delay2)
-
-		finally:
-#			print 'turning off motors'
-			#TURN OFF! SO MOTOR AND CONTROLLER DON'T OVERHEAT!!
-			if turnOff:
-				GPIO.output(motor1_A_1_pin, 0)
-				GPIO.output(motor1_B_1_pin, 0)
-				GPIO.output(motor1_A_2_pin, 0)
-				GPIO.output(motor1_B_2_pin, 0)
-
-				GPIO.output(motor2_A_1_pin, 0)
-				GPIO.output(motor2_B_1_pin, 0)
-				GPIO.output(motor2_A_2_pin, 0)
-				GPIO.output(motor2_B_2_pin, 0)
-			
-			
-
-
 try:
 	main()
 except Exception as e:
 	print(e)
-finally:
-	if isRpi:
-		setStep1(0,0,0,0)
-        #TODO turn motor 2 off
-
     
