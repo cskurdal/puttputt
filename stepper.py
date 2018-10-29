14436cbfbf05e75f588e1ece89b27fdda8e6ecad
Caleb Skurdal <cskurdal@users.noreply.github.com>
2018-10-28 21:30:55 -0700

    Update stepper.py

--- stepper.py
+++ stepper.py
@@ -25,10 +25,10 @@ class Stepper(Motor):
             raise Exception('pins must be length of 4')
         
         #Set mode
-        if mode == 'BOARD':            
-            GPIO.setmode(GPIO.BOARD)
-        else:
-            GPIO.setmode(GPIO.BCM)
+        #if mode == 'BOARD':            
+        #    GPIO.setmode(GPIO.BOARD)
+        #else:
+        GPIO.setmode(GPIO.BCM)
 
         for p in self._pins:
             GPIO.setup(p, GPIO.OUT)
